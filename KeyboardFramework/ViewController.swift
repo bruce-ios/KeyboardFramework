46822dd23f34de51c6230ce70c036aa226dc6f2a
Bruce Chen <bruce@mitake.com.tw>
2024-02-23 14:30:58 +0800

    [修改] init 專案

---
+++ KeyboardFramework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KeyboardFramework
+//
+//  Created by KC on 2024/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
